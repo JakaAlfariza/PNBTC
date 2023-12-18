45285743bc92a8ee56bd145c4d526de8aa4409f3
BamesJond700bit <121757866+BamesJond700bit@users.noreply.github.com>
2023-12-18 11:42:18 +0800

    admin gorengan6

--- application/views/Vadmin.php
+++ application/views/Vadmin.php
@@ -14,15 +14,17 @@
 
 </head>
 <body>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="<?= base_url('images/pnb.png');?>" width="40" height="40" alt="">
-      </a>
+  <a class="navbar-brand" href="#">
+    <img src="<?= base_url('images/pnb.png');?>" width="40" height="40" alt="">
+  </a>
 
       <ul class="navbar-nav font-weight-bold ml-auto">
-    
+
         <li class="nav-item">
-          <a class="nav-link" " href="<?= base_url('cadmin/logout');?>"> <i class="fa-solid fa-sign-out"></i> 
+          <a class="nav-link" href="<?= base_url('cadmin/logout');?>"> <i class="fa-solid fa-sign-out"></i> 
 		  log out</a>
         </li>
       </ul>
@@ -30,72 +32,100 @@
 
     
     <div class="container-fluid page-body-wrapper" style="margin-top: 30px;">
-			
 			<div class="main-panel">
 				<div class="content-wrapper">
 					<div class="page-header">
 					
-					</div>
-					<div class="row">
-						<div class="col-lg-5 grid-margin stretch-card">
+					<!-- </div> -->
+					<!-- <div class="row">
+						<div class="col-lg-5 grid-margin stretch-card"> -->
 							<div class="card">
-								<div class="card-body" >
-									<h4 class="card-title">Tambah data Event/lomba</h4>
-            <form class="forms-sample" method="POST" action="<?php echo base_url('cadmin/simpandata'); ?>">
-										<div class="form-group">
-											<label for="exampleInputName1">Nama event:</label>
-											<input type="text" name="nama_event" class="form-control"   required>
+								<div class="card-body">
+									<h4 class="card-title d-flex justify-content-center">Tambah data Event/lomba</h4>
+                  <div class="row">
+                  <div class="col-6">
+                  <form class="forms-sample" method="POST" action="<?php echo base_url('cadmin/simpandata'); ?>">
+                  <div class="mb-3">    
+                   <div class="form-label">
+											<label for="exampleInputName1" class="form-label">Nama event:</label>
+											<input type="text" name="nama_event" class="form-control" style="font-size:17px"  required>
 										</div>
+                  </div>
+                  <div class="mb-3">
 										<div class="form-group">
-											<label for="exampleInputName1">Gambar :</label>
+											<label for="exampleInputName1" class="form-label">Gambar :</label>
 											<input type="file" name="gambar" class="form-control"  required>
 										</div>
+                  </div>
+                  <div class="mb-3">
 										<div class="form-group">
-                                            <label for="exampleInputName1">Penyelenggara :</label>
+                      <label for="exampleInputName1" class="form-label">Penyelenggara :</label>
 											<input type="text" name="penyelenggara" class="form-control"   required>
 										</div>
-										<div class="form-group">
-                                        <label for="exampleInputName1">tanggal Awal :</label>
+                  </div>
+                  <div class="mb-3">
+                    <div class="form-group">
+                      <label for="exampleInputName1" class="form-label">tanggal Awal :</label>
 											<input type="date" name="tgl_awal" class="form-control"   required>
 										</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">tanggal Akhir :</label>
+                  </div>
+                  <div class="mb-3">     
+                    <div class="form-group">
+                      <label for="exampleInputName1" class="form-label">tanggal Akhir :</label>
 											<input type="date" name="tgl_akhir" class="form-control"  required>
 										</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">tanggal Event :</label>
+                  </div>
+                </div>
+              <div class="col-6">
+                <div class="mb-3">
+                    <div class="form-group">
+                      <label for="exampleInputName1" class="form-label">tanggal Event :</label>
 											<input type="date" name="tgl_event" class="form-control"   required>
 										</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">Harga :</label>
-											<input type="text" name="harga" class="form-control"   required>
-										</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">Lokasi :</label>
-											<input type="text" name="lokasi" class="form-control"   required>
-										</div>
+                </div>
+                <div class="mb-3">
                     <div class="form-group">
-						<label>Deskripsi :</label>
-						<textarea class="form-control" name="deskripsi" required></textarea>
-					</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">Kategori :</label>
-											<input type="text" name="kategori" class="form-control"   required>
+                      <label for="exampleInputName1" class="form-label">Harga :</label>
+											<input type="text" name="harga" class="form-control" style="font-size: 19px"  required>
 										</div>
-                                        <div class="form-group">
-                                        <label for="exampleInputName1">Link Pendaftaran :</label>
-											<input type="text" name="link_daftar" class="form-control"   required>
-										</div>
+                </div>
+                <div class="mb-3">
+                  <div class="form-group">
+                      <label for="exampleInputName1" class="form-label">Lokasi :</label>
+											<input type="text" name="lokasi" class="form-control"   required>
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <div class="form-group">
+                    <label for="exampleInputName1" class="form-label">Link Pendaftaran :</label>
+									  <input type="text" name="link_daftar" class="form-control"   required>
+								  </div>
+                </div>
+                <div class="mb-3">
+                  <div class="form-group">
+                    <label for="exampleInputName1" class="form-label">Kategori :</label>
+										<input type="text" name="kategori" class="form-control"   required>
+									</div>
+                </div>
+              </div>
+            </div> 
+                <div class="mb-3">                     
+                  <div class="form-group">
+                      <label class="form=label">Deskripsi :</label>
+                      <textarea class="form-control" name="deskripsi" required></textarea>
+                  </div>
+                </div>  
 									<button type="submit" name="submit" class="btn btn-primary me-2">Submit</button>
 									<button type="reset" class="btn btn-dark">Cancel</button>
-								</form>
+								  </form>
                 </div>
 						</div>
 					</div>
-					<div class="col-lg-7 grid-margin stretch-card">
+          
+					<div class="mt-4">
 						<div class="card">
-							<div class="card-body" style="margin-top: 20px;" >
-								<h4 class="card-title">Tabel data</h4>
+							<div class="card-body" style="margin-top: 20px;">	
+              <h4 class="card-title">Tabel data</h4>
 								</p>
 								<div class="table-responsive" >
 									<table class="table table-hover" id="tabel-data">
@@ -107,7 +137,7 @@
 												<th>Kategori</th>
 												<th>Aksi</th>
 											</tr>
-										</thead>
+									</thead>
 
                     <tbody>
              <?php
@@ -125,34 +155,32 @@
                  <td><?php echo $data->nama_event ?></td>
                  <td><?php echo $data->penyelenggara ?></td>
                  <td><?php echo $data->kategori ?></td>
-              
                  <td>
                  <button type="button" class="btn btn-sm btn-primary">Edit</button>
                      <button type="button" onclick="hapusdata
                      ('<?php echo $data->id_event ?>')" class="btn btn-sm btn-danger">Hapus</button>
                  </td>
                </tr>
-               
               <?php
                       $no++;
                       endforeach;
                  }
               ?>
-         
-         
                </tr>
              </tbody>
            </table>
-         
+          </div>
+          </div>
+          </div>
+          </div>
+          <br>
            <script>
                function hapusdata(id_event){
                    if(confirm("Apakah yakin menghapus data ini?")){
                      window.open("<?php echo base_url()?>cadmin/hapusdata/"+id_event,"_self");
                    }
                }
-           </script>
-                    
-	
+           </script>        
     <!-- Bootstrap JS and dependencies -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>

b46ac2268801d7ae9ce0da86d834cc5adfdae134
JejePunya <christianjuan169@gmail.com>
2024-01-14 23:10:20 +0800

    bugfix

--- application/models/Mprofile.php
+++ application/models/Mprofile.php
@@ -3,49 +3,36 @@ class Mprofile extends CI_Model
 {
     function simpanprofile()
     {
-        if ($this->session->userdata('role') === 'admin') {
-            $username = $this->input->post('username');
-            $email = $this->input->post('email');
-            $nama = $this->input->post('nama');
-            $password = $this->input->post('password');
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $username = $this->input->post('username');
+        $email = $this->input->post('email');
+        $nama = $this->input->post('nama');
+        $password = $this->input->post('password');
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-            $data_to_insert = array(
-                'username' => $username,
-                'email' => $email,
-                'nama' => $nama,
-                'password' => $hashed_password
-            );
+        $data_to_insert = array(
+            'username' => $username,
+            'email' => $email,
+            'nama' => $nama,
+            'password' => $hashed_password
+        );
 
-            $this->db->insert('user', $data_to_insert);
+        $this->db->insert('user', $data_to_insert);
 
-            echo "<script>alert('Data berhasil disimpan');</script>";
-            redirect('cprofile/tampilprofile', 'refresh');
-        } else {
-            $username = $this->input->post('username');
-            $email = $this->input->post('email');
-            $nama = $this->input->post('nama');
-            $password = $this->input->post('password');
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-            $data_to_insert = array(
-                'username' => $username,
-                'email' => $email,
-                'nama' => $nama,
-                'password' => $hashed_password
-            );
-
-            $this->db->insert('user', $data_to_insert);
-
-            echo "<script>alert('Data berhasil disimpan');</script>";
-            redirect('chalaman/login', 'refresh');
-        }
+        echo "<script>alert('Data berhasil disimpan');</script>";
+        redirect('cprofile/tampilakun', 'refresh');
     }
 
     function tampilakun()
     {
         $query = $this->db->get('user');
         return $query->result();
+    }
+
+    function hapusakun($id)
+    {
+    
+        $this->db->where('id', $id);
+        $this->db->delete('user');
     }
 }
 ?>
